91e26a7ec23edcdd35a68c38f50d0ce4f23648a4
2nthony <ant.hu@outlook.com>
2023-01-01 14:29:19 +0800

    fix: copyright link

--- src/components/copyright.svelte
+++ src/components/copyright.svelte
@@ -1,6 +1,6 @@
 <a
   class="text-xs text-gray-500 hover:text-sky-500 hover:underline"
-  href="https://github.com/nowidget/feedback"
+  href="https://github.com/2nthony/nofeedback"
   rel="noreferrer"
   target="_blank"
 >
